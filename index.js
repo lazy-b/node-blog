e7c4f44aaa63e77a15073dd59590a018c1dea07d
xiongyang <yang.xiong@meicloud.com>
2018-07-26 22:05:06 +0800

    add-页面设置

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ app.use(session({
 // flash 中间件，用来显示通知
 app.use(flash());
 
+// 设置模板全局常量
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+};
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user;
+  res.locals.success = req.flash('success').toString();
+  res.locals.error = req.flash('error').toString();
+  next();
+});
+
 // 路由
 routes(app);
 
